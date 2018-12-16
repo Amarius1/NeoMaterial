ccf0dd1da8ae5bf4b8dc08d8ba7c4f30048fa424
Amarius1 <amarius2000@gmail.com>
2018-12-16 17:22:56 +0200

    Add files via upload

--- js/neo-material.js
+++ js/neo-material.js
@@ -146,3 +146,46 @@ document.getElementById('dark2').onclick = function() {
 
     }
 };
+var viewportWidth = window.innerWidth-20;
+var viewportHeight = window.innerHeight-20;
+if (viewportWidth > 1000) viewportWidth = 400;
+if (viewportHeight > 500) viewportHeight = 200;
+$("#dialogEditSomeText").dialog({
+height: viewportHeight,
+width: viewportWidth,
+autoOpen: false,
+modal: false,
+resizable: false,
+draggable: false,
+position: {
+my: "center top",
+at: "center top",
+of: window,
+collision: "none"
+}
+, create: function (event, ui) {
+$(event.target).parent().css('position', 'fixed');
+}
+});
+$( "#btnClickMe" ).click(function() {
+$( "#dialogEditSomeText" ).dialog( "open" );
+});
+
+var pages = new Array('one', 'two', 'three', 'four');
+
+function toggleMenu() {
+ document.getElementsByClassName('wrapper')[0].classList.toggle('menu-open');
+}
+
+function goToPage(page) {
+  var wrapper = document.getElementsByClassName('wrapper')[0];
+  var sections = document.getElementsByTagName('section');
+  for (i = 0; i < sections.length; i++) {
+    sections[i].classList.remove('before','after');
+    if (i > page) {
+      sections[i].classList.add('after');
+    }
+  }
+  wrapper.classList.remove('menu-open','page-one','page-two');
+  wrapper.classList.add('page-' + pages[page]);
+}
