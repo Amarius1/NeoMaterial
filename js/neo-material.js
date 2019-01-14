75a525126f609da5f38ea8511c369ffb2056aaaa
Amarius1 <amarius2000@gmail.com>
2019-01-14 21:05:21 +0200

    Add files via upload

--- js/neo-material.js
+++ js/neo-material.js
@@ -353,20 +353,26 @@ $('div.nav-title:has(i)').addClass('responsive-align');
 
 
 //ACCORDION
-var acc = document.getElementsByClassName("accordion");
-var i;
+(function($) {
+    $('.accordion > li:eq(0) a.accordion-action').addClass('active').next().slideDown();
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("mouseover", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
+    $('.accordion a.accordion-action').click(function(j) {
+        var dropDown = $(this).closest('li').find('div.accordion-content');
+
+        $(this).closest('.accordion').find('div.accordion-content').not(dropDown).slideUp();
+
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).closest('.accordion').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
+
+        dropDown.stop(false, true).slideToggle();
+
+        j.preventDefault();
+    });
+})(jQuery);
 //PRELOADER
 document.addEventListener("DOMContentLoaded", function(){
 	$("#load-wrapper").delay(1200).fadeOut('slow');
